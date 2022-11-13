31cc790ecc84d5b681d878e6971cc6ff377ab69c
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-13 09:44:06 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1, 2, 5, 7, 19, 6, 1, 33 };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("[{0}] ", a[i]);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
